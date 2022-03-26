6cf600eea7228d411b86e90faee4980b760bbcee
laviscode <91470572+laviscode@users.noreply.github.com>
2022-03-26 01:50:19 -0700

    Drum-kit with spicy js

---
+++ index.js
@@ -0,0 +1,69 @@
+//Adding event listener to all the classes in .drum.
+for (var i = 0; i <8; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click",touchDrum);
+}
+
+//Detecting Button press 
+function touchDrum(){
+    var buttonInnerHtml = this.innerHTML;
+    makeSound(buttonInnerHtml);
+    buttonPressed(buttonInnerHtml);
+}
+
+//Detecting keydown press
+document.addEventListener("keydown",keyDown);
+
+function keyDown(event) {
+    makeSound(event.key);
+    buttonPressed(event.key);
+}
+
+//This function gets executed when eihter one of the button or key is pressed.
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "k":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "l":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+    
+        default:
+            console.log("Something went wrong.");
+            break;
+    }
+}
+
+//Function to change the css property when the button/key is pressed.
+function buttonPressed(pressedKey){
+    var activeButton = document.querySelector("." + pressedKey);
+   
+    activeButton.classList.add("pressed");
+    
+    setTimeout(function (){
+        activeButton.classList.remove("pressed")
+    },100);
+}
+
